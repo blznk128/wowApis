20960a40c17763c7cb4979cc5e7630a023863d2d
Benny Borunda <blznk128@yahoo.com>
2021-05-03 23:37:17 -0600

    oh lawd, got access token to come back

--- server.js
+++ server.js
@@ -10,46 +10,57 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+var btoa = require('btoa');
 
 require('./api/apiRoute')(app)
 require('./api/htmlRoute')(app);
 const CLIENT_ID = process.env.CLIENT_ID;
-    const scopes = 'wow.profile';
-    const redirectUri = 'http://localhost:3000/api/gotCode';
-    const authorizationRoute = 'https://us.battle.net/oauth/authorize';
-    const redirectUriString = encodeURIComponent(redirectUri);
-    // const scopesString = encodeURIComponent(scopes.join(' '));
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const scopes = ['wow.profile'];
+const redirectUri = 'http://localhost:3000/api/gotCode';
+const authorizationRoute = 'https://us.battle.net/oauth/authorize';
+const redirectUriString = encodeURIComponent(redirectUri);
+const TOKEN_ENDPOINT = 'https://us.battle.net/oauth/token';
+    const scopesString = encodeURIComponent(scopes.join(' '));
     app.get("/api/getAccount", (req, res) =>{
-            res.redirect(`${authorizationRoute}?client_id=${CLIENT_ID}&scope=${scopes}&redirect_uri=http://localhost:3000/api/gotCode&response_type=code`)
+            res.redirect(`${authorizationRoute}?client_id=${CLIENT_ID}&scope=${scopesString}&redirect_uri=${redirectUriString}&response_type=code`)
           })
     
     
     app.get("/api/gotCode", async function(req,res) {
         const { code } = req.query;
         console.log('this is code',code)
-        console.log("code")
+        const encodedUserInfo = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
+        const headers = {
+            authorization: `Basic ${encodedUserInfo}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        
+        var config = {
+                    method: 'POST',
+                    
+                    params: {
+                        redirect_uri: redirectUri,
+                        scope: "wow.profile",
+                        grant_type: 'authorization_code',
+                        code: code
+                    },
+                    headers
+                       
+                  }
+                     await axios(TOKEN_ENDPOINT,config)
+                  .then(  await function (response) {
+                    // const responseData =  response.json();
+                     console.log(response)
+                  })
+                  .catch(function (error) {
+                    console.log(error)
+                  });
     })
 
 
 
 
-
-
-
-
-
-// app.get("/api/getAccount", (req,res) => {
-//     // const authorizeUrl = "https://us.battle.net/oath/authorize?client_id="+CLIENT_ID+"&scope="+scopesString+"&redirect_uri="+redirectUriString+"&response_type=code";
-//     const authorizeUrl = `${AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&scope=${scopesString}&redirect_uri=${redirectUriString}&response_type=code`;
-//     res.redirect(authorizeUrl)
-//     console.log(CLIENT_ID)
-//   })
-
-//   app.get("/api/gotCode", async function(req,res) {
-//     const { code } = req.query;
-//     console.log('this is code',code)
-//     console.log("code")
-// })
 
 
 app.listen(PORT, () => {
